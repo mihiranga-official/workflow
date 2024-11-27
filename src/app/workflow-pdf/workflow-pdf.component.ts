ec3df38fba0f547970c3ada2fca7150901d737c1
Janith-Mihiranga <janith.damroit@gmail.com>
2024-11-27 15:34:38 +0530

    new changes

---
+++ src/app/workflow-pdf/workflow-pdf.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workflow-pdf',
+  standalone: true,
+  imports: [],
+  templateUrl: './workflow-pdf.component.html',
+  styleUrl: './workflow-pdf.component.scss'
+})
+export class WorkflowPDFComponent {
+
+  workflow: string = '2024-septermber'
+}
